7f9bc157d40526b7b780586e5452ebfae4ee7120
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2024-06-30 22:37:54 +0800

    chore: Add Radix UI theme configuration and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import '@radix-ui/themes/styles.css';
+import './theme-config.css'
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
