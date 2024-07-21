deb663dc10da6dc1d816d570a76ea21a4aa8131e
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2024-07-21 15:06:44 +0800

    refactor: Update BookingForm component to use zod for form validation

--- app/calendar/_components/BookingForm.tsx
+++ app/calendar/_components/BookingForm.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { bookingSchema } from '@/app/validationSchema'
 import {
   Dialog,
   DialogPanel,
@@ -5,9 +8,14 @@ import {
   Transition,
   TransitionChild,
 } from '@headlessui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { CheckCircledIcon } from '@radix-ui/react-icons'
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { MouseEventHandler } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+type BookingFormData = z.infer<typeof bookingSchema>
 
 interface props {
   showModal: boolean
@@ -16,21 +24,18 @@ interface props {
 }
 
 const BookingForm = ({ showModal, setShowModal, handleCloseModal }: props) => {
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    // setNewEvent({ ...newEvent, title: e.target.value })
-  }
+  const [error, setError] = useState('')
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    // e.preventDefault()
-    // setAllEvents([...allEvents, newEvent])
-    // setShowModal(false)
-    // setNewEvent({
-    //   title: '',
-    //   start: '',
-    //   allDay: true,
-    //   id: 0,
-    // })
-  }
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<BookingFormData>({
+    resolver: zodResolver(bookingSchema),
+  })
+
+  const onSubmit = handleSubmit(async (data) => {})
 
   return (
     <Transition.Root show={showModal} as={Fragment}>
@@ -73,37 +78,8 @@ const BookingForm = ({ showModal, setShowModal, handleCloseModal }: props) => {
                     >
                       Add Event
                     </DialogTitle>
-                    <form action='submit' onSubmit={handleSubmit}>
-                      <div className='mt-2'>
-                        <input
-                          type='text'
-                          name='title'
-                          className='block w-full rounded-md border-0 py-1.5 text-gray-900 
-                      shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
-                      focus:ring-2 
-                      focus:ring-inset focus:ring-violet-600 
-                      sm:text-sm sm:leading-6'
-                          // value={newEvent.title}
-                          onChange={(e) => handleChange(e)}
-                          placeholder='Title'
-                        />
-                      </div>
-                      <div className='mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3'>
-                        <button
-                          type='submit'
-                          className='inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25'
-                          // disabled={newEvent.title === ''}
-                        >
-                          Create
-                        </button>
-                        <button
-                          type='button'
-                          className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0'
-                          onClick={handleCloseModal}
-                        >
-                          Cancel
-                        </button>
-                      </div>
+                    <form onSubmit={onSubmit}>
+                      
                     </form>
                   </div>
                 </div>
