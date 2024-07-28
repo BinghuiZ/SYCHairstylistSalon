94c7f69c31b57f79e9d7dc21f29ef9a8b64b0971
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2024-07-28 12:16:36 +0800

    refactor: Update BookingForm component to include date and time fields

--- app/calendar/_components/BookingForm.tsx
+++ app/calendar/_components/BookingForm.tsx
@@ -2,16 +2,16 @@
 
 import ErrorMessage from '@/app/components/ErrorMessage'
 import { bookingSchema } from '@/app/validationSchema'
-import {
-  Dialog,
-  DialogPanel,
-  Transition,
-  TransitionChild,
-} from '@headlessui/react'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { CheckCircledIcon } from '@radix-ui/react-icons'
-import { Box, Button, Flex, Text, TextArea, TextField } from '@radix-ui/themes'
-import { Fragment, useState } from 'react'
+import {
+  Box,
+  Button,
+  Dialog,
+  Flex,
+  Text,
+  TextArea,
+  TextField,
+} from '@radix-ui/themes'
 import { MouseEventHandler } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -25,8 +25,6 @@ interface props {
 }
 
 const BookingForm = ({ showModal, setShowModal, handleCloseModal }: props) => {
-  const [error, setError] = useState('')
-
   const {
     register,
     control,
@@ -38,9 +36,8 @@ const BookingForm = ({ showModal, setShowModal, handleCloseModal }: props) => {
 
   const onSubmit = handleSubmit(async (data) => {})
 
-  const bookForm = () => {
-    return (
-      <form onSubmit={onSubmit}>
+  const bookForm = (
+    <form onSubmit={onSubmit}>
       <Box>
         <Text>Title</Text>
         <Controller
@@ -57,40 +54,38 @@ const BookingForm = ({ showModal, setShowModal, handleCloseModal }: props) => {
         />
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
       </Box>
-      <Box>
-        <Text>Start Date Time</Text>
-        <Controller
-          name='startDateTime'
-          control={control}
-          defaultValue={new Date()}
-          render={({ field }) => (
-            <TextField.Root
-              type='datetime-local'
-              {...register('startDateTime')}
-            />
-          )}
-        />
-        <ErrorMessage>
-          {errors.startDateTime?.message}
-        </ErrorMessage>
-      </Box>
-      <Box>
-        <Text>End Date Time</Text>
-        <Controller
-          name='endDateTime'
-          control={control}
-          defaultValue={new Date()}
-          render={({ field }) => (
-            <TextField.Root
-              type='datetime-local'
-              {...register('endDateTime')}
-            />
-          )}
-        />
-        <ErrorMessage>
-          {errors.endDateTime?.message}
-        </ErrorMessage>
-      </Box>
+      <Flex gap='2'>
+        <Box>
+          <Text>Start Date Time</Text>
+          <Controller
+            name='startDateTime'
+            control={control}
+            defaultValue={new Date()}
+            render={({ field }) => (
+              <TextField.Root
+                type='datetime-local'
+                {...register('startDateTime')}
+              />
+            )}
+          />
+          <ErrorMessage>{errors.startDateTime?.message}</ErrorMessage>
+        </Box>
+        <Box>
+          <Text>End Date Time</Text>
+          <Controller
+            name='endDateTime'
+            control={control}
+            defaultValue={new Date()}
+            render={({ field }) => (
+              <TextField.Root
+                type='datetime-local'
+                {...register('endDateTime')}
+              />
+            )}
+          />
+          <ErrorMessage>{errors.endDateTime?.message}</ErrorMessage>
+        </Box>
+      </Flex>
       <Box>
         <Text>Amount</Text>
         <Controller
@@ -121,9 +116,7 @@ const BookingForm = ({ showModal, setShowModal, handleCloseModal }: props) => {
             />
           )}
         />
-        <ErrorMessage>
-          {errors.description?.message}
-        </ErrorMessage>
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
       </Box>
       <Box>
         <Text>Client</Text>
@@ -145,59 +138,20 @@ const BookingForm = ({ showModal, setShowModal, handleCloseModal }: props) => {
         <Button type='submit'>Add</Button>
       </Flex>
     </form>
-    )
-  }
+  )
 
   return (
-    <Transition.Root show={showModal} as={Fragment}>
-      <Dialog as='div' className='relative z-10' onClose={setShowModal}>
-        <TransitionChild
-          as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
-        >
-          <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
-        </TransitionChild>
-
-        <div className='fixed inset-0 z-10 overflow-y-auto'>
-          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
-            <TransitionChild
-              as={Fragment}
-              enter='ease-out duration-300'
-              enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-              enterTo='opacity-100 translate-y-0 sm:scale-100'
-              leave='ease-in duration-200'
-              leaveFrom='opacity-100 translate-y-0 sm:scale-100'
-              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-            >
-              <DialogPanel className='relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6'>
-                <div>
-                  {/* <div className='mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100'>
-                    <CheckCircledIcon
-                      className='h-6 w-6 text-green-600'
-                      aria-hidden='true'
-                    />
-                  </div> */}
-                  <div className='mt-3 text-center sm:mt-5'>
-                    {/* <DialogTitle
-                      as='h3'
-                      className='text-base font-semibold leading-6 text-gray-900'
-                    >
-                      Add Event
-                    </DialogTitle> */}
-                    {bookForm()}
-                  </div>
-                </div>
-              </DialogPanel>
-            </TransitionChild>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+    <Dialog.Root open={showModal} onOpenChange={setShowModal}>
+      <Dialog.Content style={{ maxWidth: '640px' }}>
+        <Dialog.Title>Add New Booking</Dialog.Title>
+        <Dialog.Description size='2' mb='4'>
+          Fill out the form below to add a new booking to your list.
+        </Dialog.Description>
+        <Flex direction='column' gap='3'>
+          {bookForm}
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
   )
 }
 
