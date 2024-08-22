d946b9a57780853fc308c53dbf0c2178694a6745
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2024-08-22 22:05:22 +0800

    refactor: Add table for displaying booking history on client detail page

--- app/clients/[id]/page.tsx
+++ app/clients/[id]/page.tsx
@@ -1,7 +1,9 @@
 import prisma from '@/prisma/client'
-import { Card, Container, Flex, Heading } from '@radix-ui/themes'
+import { Card, Container, Flex, Heading, Table } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import ClientUpdateForm from '../_components/ClientUpdateForm'
+import NextLink from 'next/link'
+import { Pencil2Icon } from '@radix-ui/react-icons'
 
 interface Props {
   params: { id: string }
@@ -30,13 +32,43 @@ const ClientDetailPage = async ({ params }: Props) => {
         <Container asChild={true}>
           <Card>
             <Heading>Booking History</Heading>
-            <ul>
-              {client.bookings.map((booking) => (
-                <li key={booking.id}>
-                  {booking.id} - {booking.createdAt.toISOString()}
-                </li>
-              ))}
-            </ul>
+            <Table.Root>
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>Amount</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>Date</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>Details</Table.ColumnHeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {client.bookings.map((booking) => (
+                  <Table.Row key={booking.id}>
+                    <Table.Cell>{booking.title}</Table.Cell>
+                    <Table.Cell>{booking.amount}</Table.Cell>
+                    <Table.Cell>{booking.description}</Table.Cell>
+                    <Table.Cell>
+                      {new Date(booking.startDateTime).toLocaleDateString(
+                        'en-US',
+                        {
+                          year: 'numeric',
+                          month: 'long',
+                          day: 'numeric',
+                        }
+                      )}
+                    </Table.Cell>
+                    <Table.Cell>
+                      <NextLink
+                        href={`/clients/${client.id}/bookings/${booking.id}`}
+                      >
+                        <Pencil2Icon />
+                      </NextLink>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table.Root>
           </Card>
         </Container>
       </Flex>
