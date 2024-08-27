c0ac877a6ecc01f4dc50bca50b5401e9ea369c11
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2024-08-27 23:58:10 +0800

    refactor: sum up income

--- app/components/IncomeBarChart.tsx
+++ app/components/IncomeBarChart.tsx
@@ -11,6 +11,9 @@ interface IncomeBarChartProps {
 }
 
 const IncomeBarChart = ({ currentWeekIncome, currentMonthIncome }: IncomeBarChartProps) => {
+  const weekIncome = currentWeekIncome.reduce((acc, curr) => acc + curr, 0)
+  const monthIncome = currentMonthIncome.reduce((acc, curr) => acc + curr, 0)
+
   return (
     <Bar
       data={{
@@ -18,7 +21,7 @@ const IncomeBarChart = ({ currentWeekIncome, currentMonthIncome }: IncomeBarChar
         datasets: [
           {
             label: 'Income',
-            data: [currentWeekIncome, currentMonthIncome],
+            data: [weekIncome, monthIncome],
             backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)'],
             borderColor: ['rgba(75, 192, 192, 1)', 'rgba(54, 162, 235, 1)'],
             borderWidth: 1,
